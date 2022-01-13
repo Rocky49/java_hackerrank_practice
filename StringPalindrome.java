f4bbea0651557d786a515a3e387f36910bc70029
RockyExpo <rocky.shinde@exponentia.ai>
2022-01-13 09:53:00 +0530

    remove unused import

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,8 +1,3 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class StringPalindrome {
